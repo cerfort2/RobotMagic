58fec3d030add8b9bc8481dbddcf244197e05e6f
cerfort2 <122559164+cerfort2@users.noreply.github.com>
2024-03-05 15:15:52 -0500

    New fsm design. Need to implement state duty cycles

--- main.c
+++ main.c
@@ -11,16 +11,57 @@
 #include "..\inc\FlashProgram.h"
 #include "..\inc\Motorsimple.h"
 
+uint8_t reflectancein;
 
-// Linked data structure
+#define DARK      0x0
+#define RED       0x01      //S5=error
+#define GREEN     0x02      //S4=purchase
+#define BLUE      0x04      //s3=nickel
+#define WHITE     0x07      //s1=zero
+#define YELLOW    0x03
+#define SKYBLUE   0x06
+#define PINK      0x05      //s2=dime
+
 struct State {
-  uint32_t port2out;                // 2-bit output
-  uint32_t delay;              // time to delay in 1ms // Next if 2-bit input is 0-3 (4 inputs)
+  uint8_t port2out;                // 2-bit output
+  uint32_t duty; // time to delay in 1ms
+  uint8_t function;
 };
 
-State* next[16] = {};
 
+
+//1=FORWARD
+//2=BACKWARD
+//3=RIGHT
+//4=LEFT
+//ELSE=STOP
 typedef const struct State State_t;
+State_t fsm[21]={
+  {DARK, 4000, 5},
+  {DARK, 4000, 5},
+  {RED, 4000, 3},
+  {BLUE, 4000, 4},
+  {PINK, 4000, 4},
+  {GREEN, 4000, 1},
+  {SKYBLUE, 4000, 3},
+  {PINK, 4000, 4},
+  {SKYBLUE, 4000, 3},
+  {BLUE, 4000, 4},
+  {BLUE, 4000, 5},
+  {BLUE, 4000, 3},
+  {BLUE, 4000, 4},
+  {BLUE, 4000, 3},
+  {BLUE, 4000, 5},
+  {BLUE, 4000, 5},
+  {BLUE, 4000, 5},  //states 16-20 are sums that could possibly be reached. still need to implement
+  {BLUE, 4000, 5},
+  {BLUE, 4000, 5},
+  {BLUE, 4000, 5},
+  {BLUE, 4000, 5}
+};
+
+
+
 #define lost    &fsm[0] //lost
 #define non1      &fsm[1] //nothing
 #define veryLeft     &fsm[2] //
@@ -29,8 +70,8 @@ typedef const struct State State_t;
 #define middle    &fsm[5] //
 #define middleLeft    &fsm[6] //
 #define kindaRight    &fsm[7] //
-#define kindaleft    &fsm[8] //
-#define kindaRightMiddle     &fsm[9] //
+#define kindaLeft    &fsm[8] //
+#define kindaRightMiddle &fsm[9] //
 #define non2     &fsm[10] //
 #define kindaLeftMiddle     &fsm[11] //
 #define bigRight     &fsm[12] //
@@ -43,39 +84,29 @@ typedef const struct State State_t;
 #define big19 &fsm[19]
 #define big20 &fsm[20]
 
+State_t* next[21] = {lost,non1,veryLeft,veryRight,middleRight,middle,middleLeft,kindaRight,kindaLeft,kindaRightMiddle,non2,kindaLeftMiddle,bigRight,bigLeft,non3,bigMiddle,big16,big17,big18,big19,big20};
 
 
-//non 90 degree turns
-//90 degree turn state
 
 
-#define DARK      0x0
-#define RED       0x01      //S5=error
-#define GREEN     0x02      //S4=purchase
-#define BLUE      0x04      //s3=nickel
-#define WHITE     0x07      //s1=zero
-#define YELLOW    0x03
-#define SKYBLUE   0x06
-#define PINK      0x05      //s2=dime
+uint8_t vl,ml,mid,mr,vr,index,bumpread;
+void SysTick_Handler(void){ // every 1ms
+  // write this as part of Lab 10
 
-State_t fsm[16]={
-  {WHITE, 1},  // center
-  {RED, 1},  // left
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1},
-  {BLUE, 1}// right
-};
+
+    vl = ((reflectancein & 0x03) > 0);
+    ml = ((reflectancein & 0x0c) > 0);
+    mid= ((reflectancein & 0x18) > 0);
+    mr = ((reflectancein & 0x30) > 0);
+    vr = ((reflectancein & 0xc0) > 0);
+
+    index=vl*2 + ml*6 + mid*5 + mr*4 + vr*3;
+
+    bumpread=Bump_Read();
+
+}
+
+
 
 uint32_t Input;
 uint32_t Output;
@@ -93,7 +124,7 @@ void Port2_Init(){
     P2->OUT = 0x00;                         // all LEDs off
 }
 
-uint8_t reflectancein;
+
 int32_t position=0;
 
 int main(void){
@@ -102,59 +133,51 @@ int main(void){
     Port2_Init();
     Motor_InitSimple();
     SysTick_Init(480000,2);  // set up SysTick for 100 Hz interrupts
-   // EnableInterrupts();
+    EnableInterrupts();
     Reflectance_Init(); //initialize pins 5.3,9.2,7.0-7
     Bump_Init();
 
   State_t *pt;
-  pt=center;          //initial state
+  pt=middle;          //initial state
 
   while(1){
 
     P2->OUT=pt->port2out;
-    Clock_Delay1ms(pt->delay);   // wait
+    Clock_Delay1ms(1);   // wait
 
 
     reflectancein=Reflectance_Read(1000);
     position=Reflectance_Position(reflectancein);
-    pt = next[index];
 
 
-    if(reflectancein<0x08){ //right bits are on->go right
-      pt=pt->next[1];
-
-    }
-    else if(reflectancein>0x1F){ //left bits are on->go left
-        pt=pt->next[2];
-
-    }
-    else{
-        pt=pt->next[0];
-
-    }
 
 
-    if(pt==center){
-        Motor_ForwardSimple(4000, pt->delay);
 
-    }
-    else if(pt==left){
-           Motor_RightSimple(4000, pt->delay);
+    if(bumpread>0)
+        Motor_StopSimple();
 
-       }
-    else{
-        Motor_LeftSimple(4000, pt->delay);
-    }
+    switch(pt->function){
+    case 1:
+        Motor_ForwardSimple(pt->duty,1);
+    break;
+    case 2:
+        Motor_BackwardSimple(pt->duty,1);
+        break;
+    case 3:
+        Motor_RightSimple(pt->duty,1);
+        break;
+    case 4:
+        Motor_LeftSimple(pt->duty,1);
+        break;
+    default:
+        Motor_StopSimple();}
+
+
+
+    pt=next[index];
+
+
+
 
   }
 }
-
-// Color    LED(s) Port2
-// dark     ---    0
-// red      R--    0x01
-// blue     --B    0x04
-// green    -G-    0x02
-// yellow   RG-    0x03
-// sky blue -GB    0x06
-// white    RGB    0x07
-// pink     R-B    0x05
